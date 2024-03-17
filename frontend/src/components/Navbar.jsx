c5b4b5df3ae4fc9dea94436f74c86fdd7c730da0
kevser özeren <ozerenkevser92@gmail.com>
2024-03-17 15:07:39 +0300

    Restyle navbar and add img to it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import music from "../assets/music.jpg"
 
 const Navbar = () => {
   return (
-    <header className="bg-green-500 text-white">
-        <div className="max-w-7xl px-2 sm:px-6 lg:px-8">
-            <Link to="/" >
-                <h1>Music Store</h1>
-            </Link>
-        </div>
-    </header>
+    <nav className="flex items-center justify-between flex-wrap bg-green-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <img
+          className="fill-current h-8 w-8 mr-2 rounded-full"
+          style={{ width: 50, height: 50 }}
+          src={music}
+          alt="Music Image"
+        />
+        <span class="font-semibold text-xl tracking-tight m-2">
+          Album Store
+        </span>
+      </div>
+    </nav>
   )
 }
 
